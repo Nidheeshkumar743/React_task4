c80b8b37d881097feba1f7be8f6abce98443a327
Nidheeshkumar743 <144110412+Nidheeshkumar743@users.noreply.github.com>
2023-10-20 14:04:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React from 'react';
+import ColorPicker from './components/ColorPicker';
+
+function App() {
+
+  return (
+    <>
+
+      {/* REACT TASK-4 */}
+      <div className="App">
+        <UserList />
+      </div>
+    </>
+
+  );
+}
+
+export default App;
